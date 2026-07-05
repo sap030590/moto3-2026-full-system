78300f6cfce56ccd3ecb76a91ec3a6657e4a7737
sap030590 <127777700+sap030590@users.noreply.github.com>
2026-07-05 09:00:46 +0700

    standings-generator.js

--- standings-generator.js
+++ standings-generator.js
@@ -1,17 +1,34 @@
 const fs = require("fs-extra");
 
-// Data dummy sementara (karena results.json belum ada)
+// List Rider Moto3 2026 (dari gambar kamu)
 const results = [
-  { pos: "1", rider: "P. Acosta" },
-  { pos: "2", rider: "D. Alonso" },
-  { pos: "3", rider: "D. Holgado" },
-  { pos: "4", rider: "J. Rueda" },
-  { pos: "5", rider: "A. Fernandez" },
-  { pos: "6", rider: "I. Ortola" },
-  { pos: "7", rider: "R. Rossi" },
-  { pos: "8", rider: "T. Furusato" },
-  { pos: "9", rider: "V. Rohid" },
-  { pos: "10", rider: "J. Kelso" }
+  { pos: "1", rider: "Edie O'Shea" },
+  { pos: "2", rider: "Joel Kelso" },
+  { pos: "3", rider: "Veda Pratama" },
+  { pos: "4", rider: "Zen Mitani" },
+  { pos: "5", rider: "Adrián Fernández" },
+  { pos: "6", rider: "Guido Pini" },
+  { pos: "7", rider: "Nicola Carraro" },
+  { pos: "8", rider: "Jesús Ríos" },
+  { pos: "9", rider: "Leo Rammerstorfer" },
+  { pos: "10", rider: "Casey O'Gorman" },
+  { pos: "11", rider: "Máximo Quiles" },
+  { pos: "12", rider: "Marco Morelli" },
+  { pos: "13", rider: "Adrián Cruces" },
+  { pos: "14", rider: "Scott Ogden" },
+  { pos: "15", rider: "Cormac Buchanan" },
+  { pos: "16", rider: "Ruché Moodley" },
+  { pos: "17", rider: "Ryusei Yamanaka" },
+  { pos: "18", rider: "Hakim Danish" },
+  { pos: "19", rider: "Matteo Bertelle" },
+  { pos: "20", rider: "Joel Esteban" },
+  { pos: "21", rider: "David Almansa" },
+  { pos: "22", rider: "David Muñoz" },
+  { pos: "23", rider: "Brian Uriarte" },
+  { pos: "24", rider: "Álvaro Carpe" },
+  { pos: "25", rider: "Rico Salmela" },
+  { pos: "26", rider: "Valentin Perrone" }
+  // Tambahkan rider lain kalau ada
 ];
 
 const pointsMap = [25, 20, 16, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
@@ -20,6 +37,8 @@ const standings = {};
 
 results.forEach(r => {
   const pos = parseInt(r.pos);
+  if (isNaN(pos)) return;
+
   if (!standings[r.rider]) {
     standings[r.rider] = 0;
   }
@@ -37,4 +56,4 @@ const output = Object.keys(standings)
   .sort((a, b) => a.pos - b.pos);
 
 fs.writeJsonSync("./standings.json", output, { spaces: 2 });
-console.log("✅ Standings.json berhasil dibuat");
+console.log("✅ Standings.json berhasil dibuat dengan", output.length, "pembalap Moto3");
